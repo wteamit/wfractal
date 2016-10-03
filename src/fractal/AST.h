bc0ea09b0f7d76613e25042fd5f4d493532451d8
Daniele Lupo <jepessen@wteam.it>
2016-10-03 22:42:00 +0200

    AST files created.

---
+++ src/fractal/AST.h
@@ -0,0 +1,15 @@
+#ifndef WFRACTAL_FRACTAL_AST_H_
+#define WFRACTAL_FRACTAL_AST_H_
+
+namespace WFractal {
+namespace Fractal {
+
+class AST {
+
+};
+
+} // namespace Fractal
+} // namespace WFractal
+
+#endif // !WFRACTAL_FRACTAL_AST_H_
+
